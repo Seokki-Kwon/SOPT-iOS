8092a9a77a438de28815f5ba32a97f38c7370eea
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-02 21:09:27 +0900

    Feat: TableFooter 추가

--- ATSOPT36_Assignment/ATSOPT36_Assignment/Presentation/Main/MainViewController.swift
+++ ATSOPT36_Assignment/ATSOPT36_Assignment/Presentation/Main/MainViewController.swift
@@ -6,18 +6,14 @@
 //
 
 import UIKit
-
 import SnapKit
 
 final class MainViewController: UIViewController {
-    
-    // MARK: - Properties
-    
+
     private var prevTopOffset: CGFloat = 0
     private var topConstraint: Constraint?
     private let tableViewHeight: CGFloat = 100
-    
-    // TableView
+
     private lazy var tableView = UITableView().then {
         $0.separatorStyle = .none
         $0.contentInset.top = tableViewHeight
@@ -30,50 +26,84 @@ final class MainViewController: UIViewController {
         $0.register(BannerTableViewCell.self, forCellReuseIdentifier: BannerTableViewCell.identifier)
         $0.register(SportListCell.self, forCellReuseIdentifier: SportListCell.identifier)
         $0.register(KimGahyunBestListCell.self, forCellReuseIdentifier: KimGahyunBestListCell.identifier)
+
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
         refreshControl.tintColor = .gray
         $0.refreshControl = refreshControl
+        $0.tableFooterView = tableViewFooter
     }
-    
-    // Tableheader
+
+    private lazy var tableViewFooter = UIView(
+        frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 222)
+    ).then {
+        $0.backgroundColor = .black
+        $0.addSubview(accountInfoView)
+    }
+
+    private let accountInfoView = UIStackView().then {
+        $0.backgroundColor = .gray4
+        $0.layer.cornerRadius = 5
+        $0.isLayoutMarginsRelativeArrangement = true
+        $0.distribution = .fillProportionally
+        $0.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        $0.spacing = 8
+
+        let notiLabel = UILabel()
+        notiLabel.font = .font(.pretendardMedium, ofSize: 11)
+        notiLabel.text = "공지"
+        notiLabel.textColor = .gray2
+
+        let accountLabel = UILabel()
+        accountLabel.font = .font(.pretendardBold, ofSize: 11)
+        accountLabel.text = "티빙 계정 공유 정책 추가 안내"
+        accountLabel.textColor = .white
+
+        let moveButton = UIButton()
+        moveButton.setImage(UIImage(resource: .moveButton), for: .normal)
+
+        $0.addArrangedSubview(notiLabel)
+        $0.addArrangedSubview(accountLabel)
+        $0.addArrangedSubview(moveButton)
+    }
+
     private let headerView = UIView().then {
         $0.backgroundColor = .black
     }
-    
+
     private let headerTopView = UIView().then {
         let tvingLogo = UIImageView(image: UIImage(resource: .tvingLogo))
         let tvingCharacter = UIImageView(image: UIImage(resource: .tvingLogo2))
         let searchImage = UIImageView(image: UIImage(resource: .search))
-        
+
         tvingLogo.contentMode = .left
         tvingCharacter.contentMode = .center
         searchImage.contentMode = .center
-        
-        // 헤더 오른쪽에 들어갈 이미지
+
         let rightStackView = UIStackView()
         rightStackView.spacing = 10
         rightStackView.addArrangedSubview(searchImage)
         rightStackView.addArrangedSubview(tvingCharacter)
-        
+
         $0.addSubview(tvingLogo)
         $0.addSubview(rightStackView)
-        
-        tvingLogo.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().inset(11)
+
+        tvingLogo.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(11)
         }
-        rightStackView.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.trailing.equalToSuperview().inset(11)
+        rightStackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(11)
         }
     }
-    
+
     private let menuView = UIStackView().then { stackView in
         stackView.distribution = .equalSpacing
         stackView.backgroundColor = .black
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 11, bottom: 0, trailing: 11)
+
         ["홈", "드라마", "예능", "영화", "스포츠", "뉴스"].forEach {
             let button = UIButton()
             button.setTitleColor(.white, for: .normal)
@@ -82,71 +112,67 @@ final class MainViewController: UIViewController {
             stackView.addArrangedSubview(button)
         }
     }
-    
+
     private let topOverlay = UIView().then {
         $0.backgroundColor = .black
     }
-    
-    // MARK: - LifeCylce
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubview()
         setLayout()
         setDelegate()
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.isNavigationBarHidden = true
         tableView.contentOffset.y = -100
     }
-    
 }
 
 // MARK: - UI Setting
 
 extension MainViewController {
-    
+
     private func addSubview() {
-        [tableView, headerView, topOverlay].forEach {
-            view.addSubview($0)
-        }
-        [headerTopView, menuView].forEach {
-            headerView.addSubview($0)
-        }
+        [tableView, headerView, topOverlay].forEach { view.addSubview($0) }
+        [headerTopView, menuView].forEach { headerView.addSubview($0) }
     }
-    
+
     private func setLayout() {
-        
         headerTopView.snp.makeConstraints {
             $0.leading.trailing.top.equalToSuperview()
             $0.height.equalTo(56)
         }
-        
+
         menuView.snp.makeConstraints {
             $0.top.equalTo(headerTopView.snp.bottom)
             $0.leading.trailing.bottom.equalToSuperview()
-            
         }
-        
+
         topOverlay.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.top)
         }
-        
+
         headerView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             self.topConstraint = $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).constraint
             $0.height.equalTo(tableViewHeight)
         }
-        
+
         tableView.snp.makeConstraints {
             $0.leading.trailing.bottom.equalToSuperview()
             $0.top.equalTo(view.safeAreaLayoutGuide)
         }
+
+        accountInfoView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(10)
+            $0.top.equalToSuperview().offset(23)
+        }
     }
-    
+
     private func setDelegate() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -157,9 +183,6 @@ extension MainViewController {
 
 extension MainViewController {
     @objc private func handleRefreshControl() {
-        // Update your content…
-        
-        // Dismiss the refresh control.
         DispatchQueue.main.async {
             self.tableView.refreshControl?.endRefreshing()
         }
@@ -169,7 +192,7 @@ extension MainViewController {
 // MARK: - UITableViewDelegate
 
 extension MainViewController: UITableViewDelegate {
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.row {
         case 0:
@@ -190,27 +213,21 @@ extension MainViewController: UITableViewDelegate {
             return 100
         }
     }
-    
+
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        // 상단에 여백값 음수로 값이 들어가는 문제 방지
-        //        if scrollView.contentOffset.y < 0 {
-        //            scrollView.contentOffset.y = 0
-        //        }
         prevTopOffset = scrollView.contentOffset.y
     }
-    
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // 스크롤뷰와 컨텐츠 사이의 거리
         let currentTopOffset = scrollView.contentOffset.y
-        // 현재위치에 이전 위치를빼서 이동거리를 구한다
         let deltaY = currentTopOffset - prevTopOffset
-        
+
         if currentTopOffset > 0 {
-            // topConstraint값 가져오기
             let oldConstraint = topConstraint?.layoutConstraints[0].constant ?? 0
             topConstraint?.update(offset: max(-56, min(0, oldConstraint - deltaY)))
             view.layoutIfNeeded()
         }
+
         prevTopOffset = currentTopOffset
     }
 }
@@ -218,34 +235,55 @@ extension MainViewController: UITableViewDelegate {
 // MARK: - UITableViewDataSource
 
 extension MainViewController: UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         switch indexPath.row {
         case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.identifier, for: indexPath) as? ImageTableViewCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: ImageTableViewCell.identifier,
+                for: indexPath
+            ) as? ImageTableViewCell ?? UITableViewCell()
+
         case 1:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: TodayTvingCell.identifier, for: indexPath) as? TodayTvingCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: TodayTvingCell.identifier,
+                for: indexPath
+            ) as? TodayTvingCell ?? UITableViewCell()
+
         case 2:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: PopularListCell.identifier, for: indexPath) as? PopularListCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: PopularListCell.identifier,
+                for: indexPath
+            ) as? PopularListCell ?? UITableViewCell()
+
         case 3:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: PopularMovieListCell.identifier, for: indexPath) as? PopularMovieListCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: PopularMovieListCell.identifier,
+                for: indexPath
+            ) as? PopularMovieListCell ?? UITableViewCell()
+
         case 4:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: BannerTableViewCell.identifier, for: indexPath) as? BannerTableViewCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: BannerTableViewCell.identifier,
+                for: indexPath
+            ) as? BannerTableViewCell ?? UITableViewCell()
+
         case 5:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: SportListCell.identifier, for: indexPath) as? SportListCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: SportListCell.identifier,
+                for: indexPath
+            ) as? SportListCell ?? UITableViewCell()
+
         case 6:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: KimGahyunBestListCell.identifier, for: indexPath) as? KimGahyunBestListCell else { return UITableViewCell() }
-            return cell
+            return tableView.dequeueReusableCell(
+                withIdentifier: KimGahyunBestListCell.identifier,
+                for: indexPath
+            ) as? KimGahyunBestListCell ?? UITableViewCell()
+
         default:
             return UITableViewCell()
         }
